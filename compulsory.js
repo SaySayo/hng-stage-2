74c6be657e877c2b7a03565d69d1e1dd2d703339
SaySayo <sayobamigbade@gmail.com>
2021-08-21 10:40:33 +0100

    first commit

---
+++ compulsory.js
@@ -0,0 +1,10 @@
+var myName = {
+    firstName: "Busayo", 
+    lastName: "Bamigbade"
+}
+const myFunc = (firstName, lastName) => {
+    if(firstName !== "", lastName !== "") {
+    return "Hello there! My name is " + firstName + " " + lastName + "."
+    }
+}
+console.log(myFunc(myName.firstName, myName.lastName));
